cbe629f752386d9a634448c02b75b0763e5e1fe1
Xin Yu <chuaxc@unimelb.edu.au>
2023-03-23 16:26:45 +1100

    Added HTML Basic MVP

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ npm install
 ```
 npm run dev
 ```
+
+* Delete everything in `App.jsx`
+* 
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-import TodoItems from "./TodoItems.jsx";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const createTodo = (title) => {
+    const newTodo = {
+      id: todos.length + 1,
+      title: title,
+      done: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleDone = (id) => {
+    const todoId = todos.findIndex((entry) => entry.id === id);
+    todos[todoId].done = !todos[todoId].done;
+    setTodos([...todos]);
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodo = todos.filter((entry) => entry.id !== id);
+    setTodos(updatedTodo);
+  };
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      setTodos(savedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  }, [todos]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <TodoItems />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>To Do List App</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (title === "") {
+            alert("Cannot add an empty task");
+            return;
+          }
+          createTodo(title);
+          setTitle("");
+        }}
+      >
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.currentTarget.value);
+          }}
+          placeholder="Enter a new task"
+        ></input>
+        <button type="submit">New Entry</button>
+      </form>
+      <ul>
+        {todos.map((entry, i) => (
+          <li key={entry.id} className={entry.done ? "done" : ""}>
+            <input type="checkbox" onChange={() => toggleDone(entry.id)} />
+            {i}. {entry.title}
+            <button
+              onClick={() => {
+                deleteTodo(entry.id);
+              }}
+            >
+              🗑️
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
