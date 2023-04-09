276a9d88ca610aa34bbd169ddd0cc4122c7c534f
Xin Yu <chuaxc@unimelb.edu.au>
2023-04-09 15:33:40 +1000

    Added CSS and moved documentation

--- README.md
+++ README.md
@@ -1,29 +1,3 @@
-# todo-list-workshop
+# Web Development and Deployment Workshop
 
-1. [Setup](./docs/1-setup.md)
-
-   - GitHub Repository
-   - Text Editor
-
-2. [HTML](./docs/2-html.md)
-
-   - Ingredients for the Web
-   - HTML Page Structure
-   - Anatomy of HTML Elements
-   - HTML Tags Cheatsheet
-
-3. [CSS](./docs/3-css.md)
-
-   - CSS Syntax
-   - Displaying and Positioning
-
-4. [Building the To Do List App: React and JavaScript](docs/4-todolistapp.md)
-
-   - Creating a React Project with Vite
-   - Intro to React 
-   - Writing the HTML
-   - Making it pretty with CSS
-   - React States with `useState`
-   - JavaScript Functions
-
-5. [Deployment](docs/5-deployment.md)
+* Workshop Resources: https://chuahxinyu.github.io/notes/web-dev-workshop
--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,40 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  // Declare state variables todos and title using useState hook
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState("");
 
+  // Define function to create a new todo item
   const createTodo = (title) => {
     const newTodo = {
       id: todos.length + 1,
       title: title,
       done: false,
     };
+    // Add newTodo to the todos state array using setTodos
     setTodos([...todos, newTodo]);
   };
 
+  // Define function to toggle the 'done' property of a todo item
   const toggleDone = (id) => {
+    // Find the index of the todo item with the matching id
     const todoId = todos.findIndex((entry) => entry.id === id);
+    // Toggle the 'done' property of the todo item
     todos[todoId].done = !todos[todoId].done;
+    // Update the todos state array using setTodos
     setTodos([...todos]);
   };
 
+  // Define function to delete a todo item
   const deleteTodo = (id) => {
+    // Remove the todo item with the matching id from the todos state array
     const updatedTodo = todos.filter((entry) => entry.id !== id);
+    // Update the todos state array using setTodos
     setTodos(updatedTodo);
   };
 
+  // Load todos from localStorage when the component mounts using useEffect hook
   useEffect(() => {
     const savedTodos = JSON.parse(localStorage.getItem("todos"));
     if (savedTodos) {
@@ -32,6 +43,7 @@ function App() {
     }
   }, []);
 
+  // Save todos to localStorage when the todos state array changes using useEffect hook
   useEffect(() => {
     if (todos.length > 0) {
       localStorage.setItem("todos", JSON.stringify(todos));
@@ -39,6 +51,7 @@ function App() {
   }, [todos]);
 
   return (
+    // Render the component content
     <div className="App">
       <h1>To Do List App</h1>
       <form
@@ -58,14 +71,18 @@ function App() {
             setTitle(e.currentTarget.value);
           }}
           placeholder="Enter a new task"
+          type="text"
         ></input>
         <button type="submit">New Entry</button>
       </form>
       <ul>
+        {/* Map over the todos state array and render a list item for each todo */}
         {todos.map((entry, i) => (
           <li key={entry.id} className={entry.done ? "done" : ""}>
             <input type="checkbox" onChange={() => toggleDone(entry.id)} />
-            {i}. {entry.title}
+            <span className="title">
+              {i + 1}. {entry.title}
+            </span>
             <button
               onClick={() => {
                 deleteTodo(entry.id);
